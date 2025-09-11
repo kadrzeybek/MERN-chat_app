9264158e5c2442d23f91bfe43974c3935c56037e
kadrzeybek <kadrzeybek@kadrzeybek-MacBook-Air.local>
2025-09-11 14:26:01 +0300

    frontend setup completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import { connectDB } from './lib/db.js'
 
@@ -14,6 +15,11 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}
+))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
---
+++ frontend/src/pages/profilePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const profilePage = () => {
+  return (
+    <div>
+      <h2>Profile Page</h2>
+    </div>
+  )
+}
+
+export default profilePage
