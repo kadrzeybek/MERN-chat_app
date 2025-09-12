982a3a03f7ea2f13722d715d8938cff60c391e02
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-09-13 01:50:32 +0300

    profile page is completed

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import { generateToken } from '../lib/utils.js';
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
-import cloudinary from 'cloudinary'
+import cloudinary from '../lib/cloudinary.js'
 
 export const signup = async (req, res) =>{
   const {fullName, email, password} =  req.body
@@ -87,24 +87,28 @@ export const logout = (req, res) => {
   }
 }
 
-export const updateProfile = async (req,res) => {
+export const updateProfile = async (req, res) => {
   try {
-    const {profilePic} = req.body;
+    const { profilePic } = req.body;
     const userId = req.user._id;
 
     if (!profilePic) {
-      return res.status(400).json({message:"Profile pic is required"})
+      return res.status(400).json({ message: "Profile pic is required" });
     }
 
-    const uploadREsponse = await cloudinary.UploadStream.upload(profilePic)
-    const updatedUser = await User.findByIdAndUpdate(userId, {profilePic:uploadREsponse.secure_url}, {new:true})
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
 
-    res.status(200).json(updatedUser)
+    res.status(200).json(updatedUser);
   } catch (error) {
-      console.log("Error in update profile controller: ", error);
-      res.status(500).json({message: "Interval server error"})
+    console.log("error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
-}
+};
 
 export const checkAuth = (req, res) =>{
   try {
