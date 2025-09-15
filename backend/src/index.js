58495ba8a6ea7daa9bbd88cc6aa55e0f54ef9ad9
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-09-16 00:28:13 +0300

    deployment settings completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,8 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 
+import path from 'path'
+
 import { connectDB } from './lib/db.js'
 
 import { app, server } from './lib/socket.js'
@@ -15,6 +17,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5001
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -24,7 +28,15 @@ app.use(cors({
 ))
 
 app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV ==="prodcution"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    
+    app.get("*",(req,res) =>{
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, () =>{
     console.log('Server is running on port:',PORT);
