7e2af0c58c6cacdeb098d692ad9a270e6adeafe9
Kadir Zeybekoğlu <kadrzeybek@gmail.com>
2025-09-16 01:33:48 +0300

    regex error fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,13 +29,15 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-    
-    app.get("/.*/",(req,res) =>{
-        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"))
-    })
-}
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    // Express 5 uyumlu SPA fallback
+    app.get(/.*/, (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+    });
+  }
+  
 
 server.listen(PORT, () =>{
     console.log('Server is running on port:',PORT);
